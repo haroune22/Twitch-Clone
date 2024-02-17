c2d5f11f10241bf922825b5f07df34b9fc5bb272
haroune22 <harounehamenniche123@gmail.com>
2024-02-17 12:05:40 +0100

    keys settings

---
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { UrlCard } from './_components/UrlCard'
+import { getSelf } from '@/lib/auth-service'
+import { getStreamByUserId } from '@/lib/stream-service'
+import { KeyCard } from './_components/KeyCard'
+
+const KeysPage = async() => {
+
+  const self = await getSelf();
+
+  const stream = await getStreamByUserId(self.id)
+
+  if(!stream){
+    throw new Error("Stream not found")
+  };
+
+  return (
+    <div className='p-6'>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">
+          Keys & URLs
+        </h1>
+        <Button variant="primary">
+          Generate 
+        </Button>
+      </div>
+      <div className="space-y-4">
+        <UrlCard value={stream.serverUrl} />
+        <KeyCard value={stream.streamKey} />
+      </div>
+    </div>
+  )
+}
+
+export default KeysPage
